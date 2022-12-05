2a31f05efc8e5c8f0a7d4b7f9f707ec6ad368233
issamHosni <issamhosni08@gmail.com>
2022-12-05 21:27:33 +0100

    working on the popUp and some issue on fetch

--- code.js
+++ code.js
@@ -18,3 +18,25 @@ choixs.forEach((choix) => {
       });
    });
 });
+
+document.querySelector("form").onsubmit = (e) => {
+   e.preventDefault();
+   // let popUp = document.querySelector(".popUp .ctnt p");
+   // let popUpExit = document.querySelector(".popUp img");
+   // let matricule = document.getElementById("matricule");
+   // let code = document.getElementById("code");
+
+   (async function getData() {
+      let response = await fetch(
+         `data_handling.php?matricule=${matricule.value}&code=${code.value}`
+      );
+      let result = await response.json();
+      console.log(result);
+      // popUp.textContent = result;
+   })();
+   // setTimeout(() => {
+   //    document.querySelector(".popUp").style.cssText = "display: block";
+   //    document.querySelector(".popUp").style.cssText =
+   //       "animation: pop 0.5s forwards;";
+   // }, 300);
+};
