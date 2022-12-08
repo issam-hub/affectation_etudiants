48bb2c0ee8305b30a334268947c9970372f07a3a
issamHosni <issamhosni08@gmail.com>
2022-12-08 21:02:59 +0100

    handle connected or not for the agent

--- code.js
+++ code.js
@@ -1,22 +1,22 @@
 let choixs = document.querySelectorAll("select");
 
 choixs.forEach((choix) => {
-  choix.addEventListener("change", (e) => {
-    choixs.forEach((chx) => {
-      if (
-        chx.previousElementSibling.textContent !==
-        e.target.previousElementSibling.textContent
-      ) {
-        document
-          .querySelectorAll("." + chx.className + " > option")
-          .forEach((opt) => {
-            if (e.target.value === opt.getAttribute("value")) {
-              opt.remove();
-            }
-          });
-      }
-    });
-  });
+   choix.addEventListener("change", (e) => {
+      choixs.forEach((chx) => {
+         if (
+            chx.previousElementSibling.textContent !==
+            e.target.previousElementSibling.textContent
+         ) {
+            document
+               .querySelectorAll("." + chx.className + " > option")
+               .forEach((opt) => {
+                  if (e.target.value === opt.getAttribute("value")) {
+                     opt.remove();
+                  }
+               });
+         }
+      });
+   });
 });
 
 let popUp = document.querySelector(".popUp .ctnt p");
@@ -28,26 +28,35 @@ let gi_choix = document.getElementById("gi_choix");
 let rt_choix = document.getElementById("rt_choix");
 
 document.querySelector("form").onsubmit = (e) => {
-  e.preventDefault();
+   e.preventDefault();
 
-  (async function getData() {
-    let response = await fetch(
-      `choose_speciality.php?matricule=${matricule.value}&code=${code.value}` +
-        `&gl_choix=${gl_choix.value}` +
-        `&gi_choix=${gi_choix.value}` +
-        `&rt_choix=${rt_choix.value}`
-    );
-    let result = await response.text();
-    console.log(result);
-    popUp.textContent = result;
-  })();
-  setTimeout(() => {
-    document.querySelector(".popUp").style.cssText = "display: block";
-    document.querySelector(".popUp").style.cssText =
-      "animation: pop 0.5s forwards;";
-  }, 300);
+   (async function getData() {
+      let response = await fetch(
+         `choose_speciality.php?matricule=${matricule.value}&code=${code.value}` +
+            `&gl_choix=${gl_choix.value}` +
+            `&gi_choix=${gi_choix.value}` +
+            `&rt_choix=${rt_choix.value}`
+      );
+      let result = await response.json();
+      console.log(result);
+      if (result.status === "CONNECTED") {
+         let ul = document.createElement("ul");
+         let li = document.querySelector("li");
+         popUp.textContent = result.status;
+         // popUp.textContent = result.status;
+      } else if (result.status === "NOT_CONNECTED") {
+         let p = document.createElement("p");
+         p.style.cssText = "color: red; font-weight: bold";
+         document.querySelector("form").append(p);
+      }
+   })();
+   setTimeout(() => {
+      document.querySelector(".popUp").style.cssText = "display: block";
+      document.querySelector(".popUp").style.cssText =
+         "animation: pop 0.5s forwards;";
+   }, 300);
 };
 
 popUpExit.addEventListener("click", () => {
-  document.querySelector(".popUp").style.cssText = "display: none; opacity: 0";
+   document.querySelector(".popUp").style.cssText = "display: none; opacity: 0";
 });
