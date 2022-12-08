9660bfc52f81968e63681ad08e5a5a39adabeae7
larbiabderrahmane <larbiabderrahmane33@gmail.com>
2022-12-08 13:26:50 +0100

    cookie column removed

--- TO BE REMOVED/create_db and table.sql
+++ TO BE REMOVED/create_db and table.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE Affectation_Etudiants;
+USE Affectation_Etudiants;
 CREATE TABLE Etudiant(
 	matricule VARCHAR(255) NOT NULL PRIMARY KEY,
     nom_prenom VARCHAR(255) NOT NULL,
@@ -10,7 +11,6 @@ CREATE TABLE Etudiant(
     satisfaction VARCHAR(255) DEFAULT "non satisfait",
     choisit INT NOT NULL DEFAULT 0,
     mot_de_passe VARCHAR(8) NOT NULL DEFAULT "e_jkZe23",
-    cookie VARCHAR(255) DEFAULT NULL,
     CONSTRAINT check_ordre_GL CHECK(ordre_GL IN (0, 1, 2, 3)),
     CONSTRAINT check_ordre_GI CHECK(ordre_GI IN (0, 1, 2, 3)),
     CONSTRAINT check_ordre_RT CHECK(ordre_RT IN (0, 1, 2, 3)),
