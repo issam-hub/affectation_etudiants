68379b45345284dcf386c303055b944f02f18f69
larbiabderrahmane <larbiabderrahmane33@gmail.com>
2022-12-09 11:03:00 +0100

    adding number of nth choice affected student

--- stats.js
+++ stats.js
@@ -4,6 +4,7 @@ async function fire() {
   console.log(json);
   let table = document.querySelector("table");
   let i = 0;
+  let nChoiceAffected = [0, 0, 0];
   for (row of json) {
     table.innerHTML += `
        <tr>
@@ -11,9 +12,9 @@ async function fire() {
          <td>${row["nom"]} ${row["prenom"]}</td>
          <td>${row["matricule"]}</td>
          <td>${row["mgc"]}</td>
-         <td class='gl'>${row["ordre_gl"]}</td>
-         <td class='gi'>${row["ordre_gi"]}</td>
-         <td class='rt'>${row["ordre_rt"]}</td>
+         <td class='gl' style='font-weight: bold;color: gray'>${row["ordre_gl"]}</td>
+         <td class='gi' style='font-weight: bold;color: gray'>${row["ordre_gi"]}</td>
+         <td class='rt' style='font-weight: bold;color: gray'>${row["ordre_rt"]}</td>
          <td class='final'>${row["voeu affecte"]}</td>
          <td class="sat">${row["satisfaction"]}</td>    
       </tr>`;
@@ -25,20 +26,36 @@ async function fire() {
     console.log(final, choices);
     for (let j = 1; j < choices.length; j++) {
       if (choices[j] == final) {
+        nChoiceAffected[j - 1]++;
         console.log(
           document.getElementsByClassName(choices[j].toLowerCase())[i - 1]
         );
         document.getElementsByClassName(choices[j].toLowerCase())[
           i - 1
         ].style.backgroundColor = "green";
+        document.getElementsByClassName(choices[j].toLowerCase())[
+          i - 1
+        ].style.color = "white";
         break;
       } else
         document.getElementsByClassName(choices[j].toLowerCase())[
           i - 1
         ].style.backgroundColor = "red";
+      document.getElementsByClassName(choices[j].toLowerCase())[
+        i - 1
+      ].style.color = "white";
     }
   }
+  console.log("numbers :", nChoiceAffected);
+  /* Start affecting number of students who get their nth choice */
+  let firstChoice = nChoiceAffected[0];
+  let secondChoice = nChoiceAffected[1];
+  let thirdChoice = nChoiceAffected[2];
+
+
+  /* End affecting number of students who get their nth choice */
+
 }
 
 fire();
-//  <td>${row["voeu choisit"]}</td>
+
